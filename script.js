49b1a87a1bf10134b45d0dfbc53ea4bf75bb806d
GuiSonsin <sonsinguilherme@gmail.com>
2025-01-30 10:57:11 -0300

    Adicionando o método de remover item da lista

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const buttonAddItem = document.querySelector(".js-buttonAddItem")
 function addItemInList(){
     let nameItem = inputItem.value.trim()
 
-    // check if the item name is null
+    // checking if the item name is null
     if(nameItem === ""){
         return alert("Insira o nome do item!")
     }
 
-    // add new element to html
+    // adding new element to html
     const itemList = document.createElement("div")
-    // add class to new element
+    // adding class to new element
     itemList.classList.add("container__purchase")
 
     itemList.innerHTML = `
@@ -20,16 +20,27 @@ function addItemInList(){
     <p>${nameItem}</p>
     <img src="images/Lixeira.png" alt="Excluir" class="delete-btn">`
 
-    // add item to list
+    // adding item to list
     listItem.appendChild(itemList)
     inputItem.value = ""
+
+    // creating delete button to remove item of list
+    const deleteButton = itemList.querySelector(".delete-btn")
+
+    // adding event in the delete button created
+    deleteButton.addEventListener("click", () => deleteItem(itemList))
 }
 
-// add event in the button to show item in list
+// adding event in the button to show item in list
 buttonAddItem.addEventListener("click", addItemInList)
 
-// add event in the input to show item in list
+// adding event in the input to show item in list
 inputItem.addEventListener("keypress", (k) => {
     if(k.key === "Enter")
         addItemInList()
 })
+
+function deleteItem(item){
+    // removing item of list
+    item.remove()
+}
