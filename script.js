52818d0b0494b939999a76aae6fdd76f3ad2466a
GuiSonsin <sonsinguilherme@gmail.com>
2025-01-30 10:44:48 -0300

    Criando evento para inserir item na lista com o Enter

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ const buttonAddItem = document.querySelector(".js-buttonAddItem")
 function addItemInList(){
     let nameItem = inputItem.value.trim()
 
+    // check if the item name is null
     if(nameItem === ""){
         return alert("Insira o nome do item!")
     }
 
+    // add new element to html
     const itemList = document.createElement("div")
+    // add class to new element
     itemList.classList.add("container__purchase")
 
     itemList.innerHTML = `
@@ -17,11 +20,16 @@ function addItemInList(){
     <p>${nameItem}</p>
     <img src="images/Lixeira.png" alt="Excluir" class="delete-btn">`
 
+    // add item to list
     listItem.appendChild(itemList)
     inputItem.value = ""
-
-
 }
 
-// add event in the button to show item
+// add event in the button to show item in list
 buttonAddItem.addEventListener("click", addItemInList)
+
+// add event in the input to show item in list
+inputItem.addEventListener("keypress", (k) => {
+    if(k.key === "Enter")
+        addItemInList()
+})
